620d908bb01ab61dbc6b54d68994edb0cb31d68a
vaseemGit7 <105648020+vaseemGit7@users.noreply.github.com>
2024-09-09 14:27:32 +0530

    Create calculator module with add functionality

---
+++ modules/calculator.js
@@ -0,0 +1,19 @@
+const isValidArgument = (num1, num2) => {
+  if (typeof num1 !== "number" || typeof num2 !== "number") {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const add = (num1, num2) => {
+  if (isValidArgument(num1, num2)) {
+    return num1 + num2;
+  } else {
+    return "Invalid argument";
+  }
+};
+
+const calculator = { add };
+
+export default calculator;
